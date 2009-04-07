0785624f0d776c6ad30243935e4d41e950c54f1c
Nic <nicholas@wildinvention.co.uk>
2009-04-07 16:35:58 +0100

    updated image swapper for stem

--- stem/stem/javascripts/rotating_banner.js
+++ stem/stem/javascripts/rotating_banner.js
@@ -1,4 +1,4 @@
-var number_of_images = 3; // Modify this parameter to change number of images
+var number_of_images = 20; // Modify this parameter to change number of images
 
 var current_image = 1;
 var next_image = 2;
@@ -7,7 +7,7 @@ var preload = number_of_images;
 $(function(){
   $('div.random_image').css('height', $('#random_image').height() + 'px');
   $('#random_image').after($('#random_image').clone().attr('id', "random_image_swapper"));
-  $('#random_image_swapper').attr('src', $('#random_image_swapper').attr('src').replace(String(current_image), String(next_image)));
+  $('#random_image_swapper').attr('src', $('#random_image_swapper').attr('src').replace(String("pic" + current_image), String("pic" + next_image)));
   $('div.random_image').css('position','relative');
   $('#random_image_swapper').css('display', 'none').css('position', 'absolute').css('top', '0').css('left', '0');
   preloadImages();
@@ -19,7 +19,7 @@ preloadImages = function() {
       if (--preload <= 1) {
         setTimeout('changeImage()', 3000)
       }
-    }).attr('src', $('#random_image').attr('src').replace(String(current_image), String(i)));
+    }).attr('src', $('#random_image').attr('src').replace(String("pic" + current_image), String("pic" + i)));
   }
 }
 
@@ -29,7 +29,7 @@ changeImage = function() {
     $('#random_image_swapper').css('display', 'none');
     if (++current_image > number_of_images) {current_image = 1}
     if (++next_image > number_of_images) {next_image = 1}
-    $('#random_image_swapper').attr('src', $('#random_image_swapper').attr('src').replace(String(current_image), String(next_image)));
+    $('#random_image_swapper').attr('src', $('#random_image_swapper').attr('src').replace(String("pic" + current_image), String("pic" + next_image)));
   });
   setTimeout('changeImage()', 5000);
 }
